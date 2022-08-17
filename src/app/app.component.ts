141f37f0aa109f7326b9faeef8a8fe77c6798b8f
Lukasz Kipka <lukasz.kipka@pb.com>
2022-08-17 08:47:03 +0200

    351. Routing with Standalone Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @Component({
   standalone: true,
-  imports: [WelcomeComponent],
+  imports: [WelcomeComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { AppRoutingModule } from './app/app-routing.module';
 
 import { AppComponent } from './app/app.component';
 // import { AnalyticsService } from './app/shared/analytics.service';
@@ -12,5 +13,6 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     // AnalyticsService,
+    importProvidersFrom(AppRoutingModule)
   ],
 });
