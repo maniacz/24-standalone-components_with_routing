8e83a5b80adcc17fbb7e96fad0d8a6adf09a7408
Lukasz Kipka <lukasz.kipka@pb.com>
2022-08-17 08:24:26 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { WelcomeComponent } from './welcome/welcome.component';
+
+@Component({
+  standalone: true,
+  imports: [WelcomeComponent],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {}
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { enableProdMode } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { AppComponent } from './app/app.component';
+// import { AnalyticsService } from './app/shared/analytics.service';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    // AnalyticsService,
+  ],
+});
